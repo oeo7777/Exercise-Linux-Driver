b0da0ad84248dcb0c116e197bfa6a3e1b79ab12d
sam.tsai <sam.tsai@insyde.com>
2019-06-11 10:46:22 +0800

    add input parameter for module

--- hello_exercise/hello.c
+++ hello_exercise/hello.c
@@ -1,9 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+
+static char *whom = "world";
+static int howmany = 1;
+module_param(howmany, int, S_IRUGO); //此就算沒給 write 權限 (S_IRUGO|S_IWUSR) 一樣可輸入值
+module_param(whom, charp, S_IRUGO);  //或是在 module_param_array(name,type,num,perm); 才限制比較嚴格呢 ?
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	int i, len;
+	len = (howmany <=0) ? (1):(howmany);
+	for(i=0; i<howmany; i++)
+		printk(KERN_ALERT "Hello, %s\n", whom);
 	return 0;
 }
 
